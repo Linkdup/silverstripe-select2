31b55677055963ac061b9105cc99f41e24105104
Linkdup <Linkdup@users.noreply.github.com>
2023-07-13 10:26:27 +1000

    Update AjaxSelect2Field.php
    Disabled source_file_comments

--- code/AjaxSelect2Field.php
+++ code/AjaxSelect2Field.php
@@ -19,28 +19,34 @@ use Page;
  **/
 class AjaxSelect2Field extends TextField
 {
-    private static $allowed_actions = array('search');
+    private static $allowed_actions = ['search'];
 
-    protected $config = array(
+    protected $config = [
         'classToSearch' => 'SiteTree',
-        'searchFields' => array('Title'),
+        'searchFields' => ['Title'],
         'resultsLimit' => 200,
         'minimumInputLength' => 2,
         'resultsFormat' => '$Title',
         'selectionFormat' => '$Title',
         'placeholder' => 'Search...',
-        'excludes' => array(),
-        'filter' => array(),
+        'excludes' => [],
+        'filter' => [],
         'multiple' => false,
-    );
+    ];
 
-    public function Field($properties = array())
+    public function Field($properties = [])
     {
-        Requirements::javascript('silverstripe/admin: thirdparty/jquery/jquery.js');
-        Requirements::javascript('silverstripe/admin: thirdparty/jquery-entwine/dist/jquery.entwine-dist.js');
-        Requirements::javascript('sheadawson/silverstripe-select2: select2/select2.js');
-        Requirements::javascript('sheadawson/silverstripe-select2: javascript/ajaxselect2.init.js');
-        Requirements::css('sheadawson/silverstripe-select2: select2/select2.min.css');
+        // Requirements::javascript('silverstripe/admin: thirdparty/jquery/jquery.js');
+        // Requirements::javascript('silverstripe/admin: thirdparty/jquery-entwine/dist/jquery.entwine-dist.js');
+        Requirements::javascript(
+            'sheadawson/silverstripe-select2: select2/select2.js'
+        );
+        Requirements::javascript(
+            'sheadawson/silverstripe-select2: javascript/ajaxselect2.init.js'
+        );
+        Requirements::css(
+            'sheadawson/silverstripe-select2: select2/select2.min.css'
+        );
 
         return parent::Field($properties);
     }
@@ -53,36 +59,58 @@ class AjaxSelect2Field extends TextField
         $searchFields = $this->getConfig('searchFields');
 
         foreach ($searchFields as $searchField) {
-            $name = (strpos($searchField, ':') !== false) ? $searchField : "$searchField:PartialMatch";
+            $name =
+                strpos($searchField, ':') !== false
+                    ? $searchField
+                    : "$searchField:PartialMatch";
             $params[$name] = $request->getVar('term');
         }
-        $start = (int)$request->getVar('id') ? (int)$request->getVar('id') * $this->getConfig('resultsLimit') : 0;
+        $start = (int) $request->getVar('id')
+            ? (int) $request->getVar('id') * $this->getConfig('resultsLimit')
+            : 0;
 
-        $list = $list->filterAny($params)->exclude($this->getConfig('excludes'));
+        $list = $list
+            ->filterAny($params)
+            ->exclude($this->getConfig('excludes'));
         $filter = $this->getConfig('filter');
         if (count($filter) > 0) {
             $list = $list->filter($filter);
         }
         $total = $list->count();
-        $results = $list->sort(strtok($searchFields[0], ':'), 'ASC')->limit($this->getConfig('resultsLimit'), $start);
+        $results = $list
+            ->sort(strtok($searchFields[0], ':'), 'ASC')
+            ->limit($this->getConfig('resultsLimit'), $start);
 
-        $return = array(
-            'list' => array(),
-            'total' => $total
+        $return = [
+            'list' => [],
+            'total' => $total,
+        ];
+
+        $originalSourceFileComments = Config::inst()->get(
+            'SSViewer',
+            'source_file_comments'
         );
-
-        $originalSourceFileComments = Config::inst()->get('SSViewer', 'source_file_comments');
-        Config::inst()->update('SSViewer', 'source_file_comments', false);
+        // Config::inst()->set('SSViewer', 'source_file_comments', false);
         foreach ($results as $object) {
-            $return['list'][] = array(
+            $return['list'][] = [
                 'id' => $object->ID,
-                'resultsContent' => html_entity_decode(SSViewer::fromString($this->getConfig('resultsFormat'))->process($object)),
-                'selectionContent' => SSViewer::fromString($this->getConfig('selectionFormat'))->process($object)
-            );
+                'resultsContent' => html_entity_decode(
+                    SSViewer::fromString(
+                        $this->getConfig('resultsFormat')
+                    )->process($object)
+                ),
+                'selectionContent' => SSViewer::fromString(
+                    $this->getConfig('selectionFormat')
+                )->process($object),
+            ];
         }
-        Config::inst()->update('SSViewer', 'source_file_comments', $originalSourceFileComments);
+        // Config::inst()->set(
+        //     'SSViewer',
+        //     'source_file_comments',
+        //     $originalSourceFileComments
+        // );
 
-        return Convert::array2json($return);
+        return json_encode($return);
     }
 
     public function setConfig($k, $v)
@@ -102,23 +130,36 @@ class AjaxSelect2Field extends TextField
      */
     public function getAttributes()
     {
-        $attributes = array_merge(
-            parent::getAttributes(),
-            array(
-                'type' => 'hidden',
-                'data-searchurl' => $this->Link('search'),
-                'data-minimuminputlength' => $this->getConfig('minimumInputLength'),
-                'data-resultslimit' => $this->getConfig('resultsLimit'),
-                'data-placeholder' => $this->getConfig('placeholder'),
-                'data-multiple' => $this->getConfig('multiple')
-            )
-        );
+        $attributes = array_merge(parent::getAttributes(), [
+            'type' => 'hidden',
+            'data-searchurl' => $this->Link('search'),
+            'data-minimuminputlength' => $this->getConfig('minimumInputLength'),
+            'data-resultslimit' => $this->getConfig('resultsLimit'),
+            'data-placeholder' => $this->getConfig('placeholder'),
+            'data-multiple' => $this->getConfig('multiple'),
+        ]);
 
-        if ($this->Value() && $object = DataObject::get($this->getConfig('classToSearch'))->byID($this->Value())) {
-            $originalSourceFileComments = Config::inst()->get('SSViewer', 'source_file_comments');
-            Config::inst()->update('SSViewer', 'source_file_comments', false);
-            $attributes['data-selectioncontent'] = html_entity_decode(SSViewer::fromString($this->getConfig('selectionFormat'))->process($object));
-            Config::inst()->update('SSViewer', 'source_file_comments', $originalSourceFileComments);
+        if (
+            $this->Value() &&
+            ($object = DataObject::get($this->getConfig('classToSearch'))->byID(
+                $this->Value()
+            ))
+        ) {
+            $originalSourceFileComments = Config::inst()->get(
+                'SSViewer',
+                'source_file_comments'
+            );
+            // Config::inst()->update('SSViewer', 'source_file_comments', false);
+            $attributes['data-selectioncontent'] = html_entity_decode(
+                SSViewer::fromString(
+                    $this->getConfig('selectionFormat')
+                )->process($object)
+            );
+            // Config::inst()->update(
+            //     'SSViewer',
+            //     'source_file_comments',
+            //     $originalSourceFileComments
+            // );
         }
 
         return $attributes;
